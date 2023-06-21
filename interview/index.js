32c728a9f9227a5ffe489e7fb6c937d6eb33fed3
santosh <styadav018@gmail.com>
2023-06-22 00:31:50 +0530

    added example of stirct mode

---
+++ interview/index.js
@@ -0,0 +1,21 @@
+
+
+// function find(){
+//     let a = b = 0;
+//     a++;
+//     return a
+// }
+
+// find()
+
+// console.log("a typeOf" ,typeof  a)
+// console.log("b typeOf" ,typeof  b)
+
+
+function getName(){
+    'use strict'
+   name = "santosh";
+    console.log(name)
+}
+
+getName()
